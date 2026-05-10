adf95dc7d86e06a1f53f63a0a1c10714b4e6829c
glenn <glenn>
2026-05-10 23:59:32 +0100

    Added form field section toggle functionality to toggle switch component

--- _components/toggle-switch-component.js
+++ _components/toggle-switch-component.js
@@ -21,7 +21,40 @@ class ToggleSwitch extends HTMLElement {
                 <span class="toggle-track"><span class="toggle-thumb"></span></span>
             </label>
 		`;
+
+        this.initToggle();
     }
+
+    // -------------------------------------------------------------------------
+    // Events
+    // -------------------------------------------------------------------------
+
+	initToggle() {
+		const btn = document.querySelector("#"+this._inputID);
+        const group = document.querySelector("#"+this._inputID+"-fields");
+
+        if (group.classList.contains("open")) {
+            group.style.height = "auto";
+        } else {
+            group.style.height = "0px";
+        }
+
+        btn.addEventListener("click", () => {
+	        if (group.classList.contains("open")) {
+                group.style.height = group.scrollHeight + "px";
+                requestAnimationFrame(() => { group.style.height = "0px"; });
+                group.classList.remove("open");
+            } else {
+                group.classList.add("open");
+                group.style.height = group.scrollHeight + "px";
+
+                group.addEventListener("transitionend", function handler() {
+                    group.style.height = "auto";
+                    group.removeEventListener("transitionend", handler);
+                });
+            }
+        });
+	}
 }
 
 customElements.define('toggle-switch-component', ToggleSwitch);
